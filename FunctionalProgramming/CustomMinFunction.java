01b17b9a3d926afb32ae0727bb87e5243a95cb67
Dany <danijeliazkov@yahoo.bg>
2024-05-13 16:39:52 +0300

    functional programming exercises

---
+++ FunctionalProgramming/CustomMinFunction.java
@@ -0,0 +1,21 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CustomMinFunction {
+    public static void main(String[] args) {
+        Function<List<Integer>, Integer> findMin = Collections::min;
+
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        System.out.println(findMin.apply(numbers));
+    }
+}
